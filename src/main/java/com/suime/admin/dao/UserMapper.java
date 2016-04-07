1f630045d1a3c5ac70e328129d35889e58c36f61
lynch0571 <lynch0571@gmail.com>
2016-04-07 08:38:56 +0800

    添加用户列表接口

---
+++ src/main/java/com/suime/admin/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package com.suime.admin.dao;
+
+import com.confucian.mybatis.support.mapper.GenericMapper;
+import com.suime.context.model.User;
+
+/**
+ * userMapper
+ * Created by Lynch 06/04/2016.
+ */
+public interface UserMapper extends GenericMapper<User> {
+
+}
---
+++ src/main/java/com/suime/admin/service/UserService.java
@@ -0,0 +1,12 @@
+package com.suime.admin.service;
+
+import com.confucian.mybatis.support.service.GenericService;
+import com.suime.context.model.User;
+
+/**
+ * userService
+ * Created by Lynch 06/04/2016.
+ */
+public interface UserService extends GenericService<User> {
+
+}
