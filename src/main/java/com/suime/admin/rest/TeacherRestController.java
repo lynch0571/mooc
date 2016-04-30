2c770b22ac526c9edca47a085cb777cf592a77f3
lynch0571 <lynch0571@gmail.com>
2016-04-30 21:50:52 +0800

    爬取教师信息。 1、从文件或网络获取教师列表接口； 2、从文件或网络获取教师列表并插入数据库接口；

--- src/main/java/com/suime/admin/rest/TeacherRestController.java
+++ src/main/java/com/suime/admin/rest/TeacherRestController.java
@@ -1,8 +1,12 @@
 package com.suime.admin.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,10 +31,10 @@ public class TeacherRestController extends SpiderAbstractRestController {
     private TeacherService teacherService;
 
     /**
-     * 获取教师列表
+     * 从数据库中获取教师列表
      * @return
      */
-    @RequestMapping(path = "/list", method = {RequestMethod.GET})
+    @RequestMapping(path = "/list/db", method = {RequestMethod.GET})
     @RequiresAuthentication
     public Object list() {
         CommonResultBean resultBean = new CommonResultBean();
@@ -39,20 +43,95 @@ public class TeacherRestController extends SpiderAbstractRestController {
         return resultBean;
     }
     
-    @RequestMapping(path = "/add", method = {RequestMethod.POST})
+    /**
+     * 从文件中获取教师列表
+     * @param type
+     * @param idBegin
+     * @param idEnd
+     * @return
+     */
+    @RequestMapping(path = "/list/file/{type}/{idBegin}/{idEnd}", method = {RequestMethod.GET})
     @RequiresAuthentication
-    public Object add(){
+    public Object teacherListFromFile(	@PathVariable String type,
+				    					@PathVariable Integer idBegin,
+				    					@PathVariable Integer idEnd
+    				){
     	CommonResultBean resultBean = new CommonResultBean();
     	resultBean.setResult(Constants.NORMAL_RESULT_RIGHT);
-    	Teacher teacher=new Teacher();
-    	Document doc=this.getLocalDocumentByCourseId(6);
-    	
-    	teacher.setId(Integer.parseInt(this.getTeacherId(doc)));
-    	teacher.setName(this.getTeacherName(doc));
-    	teacher.setJob(this.getTeacherJob(doc));
-    	teacher.setPhoto(this.getTeacherPhoto(doc));
-    	
-    	resultBean.setBody(teacher);
+    	List<Teacher> teachers=new ArrayList<Teacher>();
+    	for(int i=idBegin;i<=idEnd;i++){
+    		Document doc=null;
+    		if(type.equals("local")){
+    			doc=this.getLocalDocumentByCourseId(i);
+    		}else if (type.equals("net")){
+    			doc=this.getNetDocumentByCourseId(i);
+    		}
+    		if(doc!=null){
+    			Teacher teacher=new Teacher();
+    			if(this.getTeacherId(doc)!=null){
+    				teacher.setId(Integer.parseInt(this.getTeacherId(doc)));
+    				teacher.setName(this.getTeacherName(doc));
+    				teacher.setJob(this.getTeacherJob(doc));
+    				teacher.setPhoto(this.getTeacherPhoto(doc));
+    				//去重
+    				boolean isExist=false;
+    				for(int j=0;j<teachers.size();j++){
+    					if(teachers.get(j).getId().equals(teacher.getId())){
+    						isExist=true;
+    						break;
+    					}
+    				}
+    				if(!isExist){
+    					teachers.add(teacher);
+    				}
+    			}
+    		}
+    	}
+    	resultBean.setBody(teachers);
     	return resultBean;
     }
+    
+    @RequestMapping(path = "/insert/file/{type}/{idBegin}/{idEnd}", method = {RequestMethod.GET})
+    @RequiresAuthentication
+    public Object insertTeachers(	@PathVariable String type,
+						    		@PathVariable Integer idBegin,
+						    		@PathVariable Integer idEnd
+					    		){
+    	CommonResultBean resultBean = new CommonResultBean();
+    	resultBean.setResult(Constants.NORMAL_RESULT_RIGHT);
+    	List<Teacher> teachers=new ArrayList<Teacher>();
+    	for(int i=idBegin;i<=idEnd;i++){
+    		Document doc=null;
+    		if(type.equals("local")){
+    			doc=this.getLocalDocumentByCourseId(i);
+    		}else if (type.equals("net")){
+    			doc=this.getNetDocumentByCourseId(i);
+    		}
+    		if(doc!=null){
+    			Teacher teacher=new Teacher();
+    			if(this.getTeacherId(doc)!=null){
+    				teacher.setId(Integer.parseInt(this.getTeacherId(doc)));
+    				teacher.setName(this.getTeacherName(doc));
+    				teacher.setJob(this.getTeacherJob(doc));
+    				teacher.setPhoto(this.getTeacherPhoto(doc));
+    				teacher.setStatus((byte) 0);
+    				//去重
+    				boolean isExist=false;
+    				for(int j=0;j<teachers.size();j++){
+    					if(teachers.get(j).getId().equals(teacher.getId())){
+    						isExist=true;
+    						break;
+    					}
+    				}
+    				if(!isExist){
+    					teachers.add(teacher);
+    					teacherService.save(teacher);
+    				}
+    			}
+    		}
+    	}
+    	resultBean.setBody(teachers);
+    	return resultBean;
+    }
+    
 }
