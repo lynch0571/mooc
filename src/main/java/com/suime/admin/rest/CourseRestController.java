7a39d22a0eb7faa195638d504419dd1c097803ed
lynch0571 <lynch0571@gmail.com>
2016-05-01 00:29:58 +0800

    课程接口 1、从本地文件或在线文档获取课程列表； 2、从本地文件或在线文档获取课程列表并插入数据库； 3、从数据库中获取课程列表；

--- src/main/java/com/suime/admin/rest/CourseRestController.java
+++ src/main/java/com/suime/admin/rest/CourseRestController.java
@@ -1,9 +1,20 @@
 package com.suime.admin.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.confucian.framework.web.AbstractRestController;
+import com.confucian.framework.dto.CommonResultBean;
+import com.confucian.framework.support.Constants;
+import com.suime.admin.helper.CourseSpiderAbstractRestController;
+import com.suime.admin.service.CourseService;
+import com.suime.context.model.Course;
 
 /**
  * courseRestController
@@ -11,6 +22,68 @@ import com.confucian.framework.web.AbstractRestController;
  */
 @RestController
 @RequestMapping("/course")
-public class CourseRestController extends AbstractRestController {
-
+public class CourseRestController extends CourseSpiderAbstractRestController {
+	
+	@Autowired
+	private CourseService courseService;
+	
+	/**
+     * 从数据库中获取课程列表
+     * @return
+     */
+    @RequestMapping(path = "/list/db", method = {RequestMethod.GET})
+    @RequiresAuthentication
+    public Object list() {
+        CommonResultBean resultBean = new CommonResultBean();
+        resultBean.setResult(Constants.NORMAL_RESULT_RIGHT);
+        resultBean.setBody(courseService.fetchSearchByPage(null, null, 0, 0));
+        return resultBean;
+    }
+    
+    /**
+     * 从文件中获取课程列表
+     * @param type
+     * @param idBegin
+     * @param idEnd
+     * @return
+     */
+    @RequestMapping(path = "/list/file/{type}/{idBegin}/{idEnd}", method = {RequestMethod.GET})
+    @RequiresAuthentication
+    public Object courseListFromFile(	@PathVariable String type,
+				    					@PathVariable Integer idBegin,
+				    					@PathVariable Integer idEnd
+    				){
+    	CommonResultBean resultBean = new CommonResultBean();
+    	resultBean.setResult(Constants.NORMAL_RESULT_RIGHT);
+    	List<Course> courses=new ArrayList<Course>();
+    	for(int i=idBegin;i<=idEnd;i++){
+    		Course course=this.getCourseById(i, type);
+    		if(course!=null){
+    			courses.add(course);
+    		}
+    	}
+    	resultBean.setBody(courses);
+    	return resultBean;
+    }
+    
+    @RequestMapping(path = "/insert/file/{type}/{idBegin}/{idEnd}", method = {RequestMethod.GET})
+    @RequiresAuthentication
+    public Object insertTeachers(	@PathVariable String type,
+						    		@PathVariable Integer idBegin,
+						    		@PathVariable Integer idEnd
+					    		){
+    	CommonResultBean resultBean = new CommonResultBean();
+    	resultBean.setResult(Constants.NORMAL_RESULT_RIGHT);
+    	List<Course> courses=new ArrayList<Course>();
+    	for(int i=idBegin;i<=idEnd;i++){
+    		Course course=this.getCourseById(i, type);
+    		if(course!=null){
+    			courses.add(course);
+    			courseService.save(course);
+    		}
+    	}
+    	resultBean.setBody(courses);
+    	return resultBean;
+    }
+    
 }
